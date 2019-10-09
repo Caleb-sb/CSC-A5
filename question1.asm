14a4d30967d6244245f1539f948ea3b633b0463b
Caleb-sb <52993870+Caleb-sb@users.noreply.github.com>
2019-10-09 11:51:06 +0200

    Question1 working

--- question1.asm
+++ question1.asm
@@ -1,9 +1,11 @@
 #Question 1: find single Digit divisors
 .data
-  input: .asciiz "\nEnter a number:"
-  output: .asciiz "\nThe single digit divisors are:"
-
+  input: .asciiz "Enter a number:\n"
+  output: .asciiz "The single digit divisors are:\n"
+  nl: .asciiz "\n"
+.text
 .globl main
+main:
   la $a0, input
   li $v0, 4
   syscall             #Ask for user input
@@ -13,18 +15,29 @@
 
   move $t0, $v0       #temp store the input int
   li $t1, 2
+  
+  la $a0, output
+  li $v0, 4
+  syscall
 
   loop:
     beq, $t1, 10, end
     div $t0, $t1
     mfhi $t3
     bne $t3, $0, increment
-    move $a0, $t1
-    li $v0, 4
+	
+	move $a0, $t1
+    li $v0, 1
     syscall
 
+	la $a0, nl
+	li $v0, 4
+	syscall 
   increment:
     addi $t1, $t1, 1
     b loop
 
   end:
+	li $v0, 10
+	syscall
+	
